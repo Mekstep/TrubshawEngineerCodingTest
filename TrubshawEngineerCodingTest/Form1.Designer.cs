e22485c8681d67b55eb801ccd25799a00f5ff996
Niall <niallswan@outlook.com>
2021-08-28 21:06:49 +0100

    Q1 functionality complete

--- TrubshawEngineerCodingTest/Form1.Designer.cs
+++ TrubshawEngineerCodingTest/Form1.Designer.cs
@@ -30,6 +30,11 @@ namespace TrubshawEngineerCodingTest
         private void InitializeComponent()
         {
             this.nextButton = new System.Windows.Forms.Button();
+            this.questionNo = new System.Windows.Forms.Label();
+            this.questionText = new System.Windows.Forms.Label();
+            this.answerEntryBox = new System.Windows.Forms.TextBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.warningLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // nextButton
@@ -40,23 +45,81 @@ namespace TrubshawEngineerCodingTest
             this.nextButton.TabIndex = 0;
             this.nextButton.Text = "Next";
             this.nextButton.UseVisualStyleBackColor = true;
-            this.nextButton.Click += new System.EventHandler(this.button1_Click);
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // questionNo
+            // 
+            this.questionNo.AutoSize = true;
+            this.questionNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionNo.Location = new System.Drawing.Point(3, 31);
+            this.questionNo.Name = "questionNo";
+            this.questionNo.Size = new System.Drawing.Size(195, 51);
+            this.questionNo.TabIndex = 1;
+            this.questionNo.Text = "Question";
+            // 
+            // questionText
+            // 
+            this.questionText.AutoSize = true;
+            this.questionText.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionText.Location = new System.Drawing.Point(4, 82);
+            this.questionText.Name = "questionText";
+            this.questionText.Size = new System.Drawing.Size(257, 44);
+            this.questionText.TabIndex = 2;
+            this.questionText.Text = "Question Text";
+            // 
+            // answerEntryBox
+            // 
+            this.answerEntryBox.Location = new System.Drawing.Point(12, 168);
+            this.answerEntryBox.Name = "answerEntryBox";
+            this.answerEntryBox.Size = new System.Drawing.Size(347, 20);
+            this.answerEntryBox.TabIndex = 3;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 387);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(168, 51);
+            this.backButton.TabIndex = 4;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // warningLabel
+            // 
+            this.warningLabel.AutoSize = true;
+            this.warningLabel.ForeColor = System.Drawing.Color.Red;
+            this.warningLabel.Location = new System.Drawing.Point(12, 143);
+            this.warningLabel.Name = "warningLabel";
+            this.warningLabel.Size = new System.Drawing.Size(70, 13);
+            this.warningLabel.TabIndex = 5;
+            this.warningLabel.Text = "warningLabel";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.warningLabel);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.answerEntryBox);
+            this.Controls.Add(this.questionText);
+            this.Controls.Add(this.questionNo);
             this.Controls.Add(this.nextButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Label questionNo;
+        private System.Windows.Forms.Label questionText;
+        private System.Windows.Forms.TextBox answerEntryBox;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Label warningLabel;
     }
 }
 
--- TrubshawEngineerCodingTest/Form1.cs
+++ TrubshawEngineerCodingTest/Form1.cs
@@ -8,18 +8,89 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Niall Swan
+
 namespace TrubshawEngineerCodingTest
 {
     public partial class Form1 : Form
     {
+
+        // Variables
+        int questionNumber = 1;
+
+        string name;
+
+
+        // Window
         public Form1()
         {
             InitializeComponent();
+            warningLabel.Visible = false;
+            askQuestion(questionNumber);
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        // Question function
+        private void askQuestion(int qNum)
         {
+            // Switch Current Question
+            switch (qNum)
+            {
+                // Q1
+                case 1:
+                    questionNo.Text = "Question 1";
+                    questionText.Text = "What is your name?";
+                    backButton.Visible = false;
+                    break;
+                // Q2
+                case 2:
+                    questionNo.Text = "Question 2";
+                    questionText.Text = "What is your email?";
+                    backButton.Visible = false;
+                    break;
+                // Q3
+                case 3:
+                    questionNo.Text = "Question 3";
+                    questionText.Text = "What level of threat are you reporting?";
+                    backButton.Visible = true;
+                    break;
+            }
+        }
 
+        
+        // Button to advance to next question
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            // Q1
+            if(questionNumber == 1)
+            {
+                // Get name from text box
+                name = answerEntryBox.Text;
+
+                // Check name
+                if(name.Length < 2 || name.Length > 25)
+                {
+                    // Display warning
+                    warningLabel.Text = "Please enter a name between 2 and 25 characters in length.";
+                    warningLabel.Visible = true;
+                }
+                // If correct proceed to Q2
+                else
+                {
+                    warningLabel.Visible = false;
+                    questionNumber = 2;
+                    askQuestion(questionNumber);
+                    answerEntryBox.Clear();
+                }
+            }
+
+        }
+
+        // Back button to return to question 2
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            questionNumber = 2;
+            askQuestion(questionNumber);
         }
     }
 }
